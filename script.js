4458a8d2dab3627863ed348590ec5cbe7b46d973
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2022-10-18 19:50:33 +0200

    commit 4

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var apiRoot = 'https://kodilla-tasks-nj.herokuapp.com/v1/tasks'
     //const apiRoot = 'http://localhost:8080/v1/tasks';
     //const trelloApiRoot = 'http://localhost:8080/v1/trello';
-    const trelloApiRoot = 'https://kodilla-tasks-nj.herokuapp.com/v1/tasks';
+    const trelloApiRoot = 'https://kodilla-tasks-nj.herokuapp.com/v1/trello';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
 
